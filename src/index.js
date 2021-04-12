63b53b62aea7d989834f2e22cf4fcef9ee8c6b61
David Johnston <djohnston@whispir.com>
2021-04-12 10:58:41 +1000

    Change to no spread

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-import Editor from "@monaco-editor/react";
+import Editor from "monaco-editor-react-no-spread";
 
 function App() {
   return (
